693e99353540b8ff46b6d69cd20da16004aa553d
aliviahossain <aliviahossainofficial@gmail.com>
2025-10-21 23:42:55 +0530

    Edited run.py

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from backend import create_app
 
+# This creates the 'app' variable that Gunicorn needs to import
 app = create_app()
+
+# This block only runs when you execute "python run.py"
+# It is skipped by Gunicorn
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
