69efef4f166c4cc26861c8659a03aa69c6b5a533
SarveshDujari <dujarisarvesh@gmail.com>
2026-05-21 21:57:03 +0530

    Add custom fallback 404 page

--- backend/__init__.py
+++ backend/__init__.py
@@ -200,4 +200,8 @@ def create_app():
         db.create_all()
         _ensure_user_profile_columns(db.engine)
 
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("404.html"), 404     
+    
     return app
